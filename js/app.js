37aed2e8034d478342098e21f63b1e9a1f747a3e
mejaz <mohdejazsiddiqui@gmail.com>
2015-04-02 01:50:21 +0530

    js updated

---
+++ js/app.js
@@ -0,0 +1,134 @@
+var homeHTML = "We have spent some time describing why we need to test, but we have not discussed\
+what testing is. What do we mean by the word testing? We use the words\
+test and testing in everyday life and earlier we said testing could be described as\
+'checking the software is OK'. That is not a detailed enough definition to help\
+us understand software testing. Let's use an analogy to help us: driving tests. In\
+a driving test, the examiner critically assesses the candidate's driving, noting\
+every mistake, large or small, made by the driver under test. The examiner takes\
+the driver through a route which tests many possible driving activities, such as\
+road junctions of different types, control and maneuvering of the car, ability to\
+stop safely in an emergency, and awareness of the road, other road users and\
+hazards. Some of the activities must be tested. For example, in the UK, an\
+emergency stop test is always carried out, with the examiner simulating the\
+moment of emergency by hitting the dashboard at which point the driver must\
+stop the car quickly, safely and without loss of control. At the end of the test,\
+the examiner makes a judgment about the driver's performance. Has the driver\
+passed the test or failed? The examiner bases the judgment on the number and\
+severity of the failures identified, and also whether the driver has been able to\
+meet the driving requirements. A single severe fault is enough to fail the whole\
+test, but a small number of minor faults might still mean the test is passed.\
+Many minor faults would reduce the confidence of the examiner in the quality\
+—of the driving to the point where the driver cannot pass.";
+
+var admissionsHTML = "Admissions to <b>Al Hadi Public School</b> starts in the month of April every year.<br /><br />\
+              We are currently taking admissions in classes from <b>Nursery</b> to <b>Class VII.</b><br /><br />\
+              <b>Admission Procedure:</b><br /><br />\
+              <ol>\
+                <li>Fill and Submit the application form, issued from the school office.</li>\
+                <li>Appear for the admission test.</li>\
+                <li>Result of the admission test is declared on the date shared on the admission test day.</li>\
+                <li>Result will be displayed on the school notice board and on the school website.</li>\
+                <li>The admission process has to be completed within two weeks of result declaration.</li>\
+              </ol>\
+              <p>For any queries, please contact us by following this link <a href='#' onclick=\"home(\'header\',\'content\', \'cnt\')\">Contact Us.</a>\
+            </div>\
+        </div>\
+      </div>";
+
+var syllabusHTML = "Books and Syllabus are mentioned below for each class:<br /><br />\
+              <ol>\
+                <li><a href='#'>Nursery</a></li><br />\
+                <li><a href='#'>K.G.</a></li><br />\
+                <li><a href='#'>First - I</a></li><br />\
+                <li><a href='#'>Second - II</a></li><br />\
+                <li><a href='#'>Third - III</a></li><br />\
+                <li><a href='#'>Fourth - IV</a></li><br />\
+                <li><a href='#'>Fifth - V</a></li><br />\
+                <li><a href='#'>Sixth - VI</a></li><br />\
+                <li><a href='#'>Seventh - VII</a></li><br />\
+            </ol>\
+              For more information regarding the syllabus or the book shop, please contact us by following this link <a href='#' onclick=\"home(\'header\',\'content\', \'cnt\')\">Contact Us</a> <br /><br />\
+            </div>\
+        </div>\
+      </div>";
+
+var galleryHTML = "Al Hadi Public School is located at a 500m distance inside Moon Palace DDC Colony. <br /><br />\
+              <img class='img-responsive' src='images/Lighthouse.jpg'><br /><br />\
+              The School has spacious classrooms.<br /><br />\
+              <img class='img-responsive' src='images/Desert.jpg'><br /><br />\
+            </div>\
+        </div>\
+      </div>";
+
+var hwtHTML = "We have spent some time describing why we need to test, but we have not discussed\
+what testing is. What do we mean by the word testing? We use the words\
+test and testing in everyday life and earlier we said testing could be described as\
+'checking the software is OK'. That is not a detailed enough definition to help\
+us understand software testing. Let's use an analogy to help us: driving tests. In\
+a driving test, the examiner critically assesses the candidate's driving, noting\
+every mistake, large or small, made by the driver under test. The examiner takes\
+the driver through a route which tests many possible driving activities, such as\
+road junctions of different types, control and maneuvering of the car, ability to\
+stop safely in an emergency, and awareness of the road, other road users and\
+hazards. Some of the activities must be tested. For example, in the UK, an\
+emergency stop test is always carried out, with the examiner simulating the\
+moment of emergency by hitting the dashboard at which point the driver must\
+stop the car quickly, safely and without loss of control. At the end of the test,\
+the examiner makes a judgment about the driver's performance. Has the driver\
+passed the test or failed? The examiner bases the judgment on the number and\
+severity of the failures identified, and also whether the driver has been able to\
+meet the driving requirements. A single severe fault is enough to fail the whole\
+test, but a small number of minor faults might still mean the test is passed.\
+Many minor faults would reduce the confidence of the examiner in the quality\
+—of the driving to the point where the driver cannot pass.";
+
+var sblHTML = "We have spent some time describing why we need to test, but we have not discussed\
+what testing is. What do we mean by the word testing? We use the words\
+test and testing in everyday life and earlier we said testing could be described as\
+'checking the software is OK'. That is not a detailed enough definition to help\
+us understand software testing. Let's use an analogy to help us: driving tests. In\
+a driving test, the examiner critically assesses the candidate's driving, noting\
+every mistake, large or small, made by the driver under test. The examiner takes\
+the driver through a route which tests many possible driving activities, such as\
+road junctions of different types, control and maneuvering of the car, ability to\
+stop safely in an emergency, and awareness of the road, other road users and\
+hazards. Some of the activities must be tested. For example, in the UK, an\
+emergency stop test is always carried out, with the examiner simulating the\
+moment of emergency by hitting the dashboard at which point the driver must\
+stop the car quickly, safely and without loss of control. At the end of the test,\
+the examiner makes a judgment about the driver's performance. Has the driver\
+passed the test or failed? The examiner bases the judgment on the number and\
+severity of the failures identified, and also whether the driver has been able to\
+meet the driving requirements. A single severe fault is enough to fail the whole\
+test, but a small number of minor faults might still mean the test is passed.\
+Many minor faults would reduce the confidence of the examiner in the quality\
+—of the driving to the point where the driver cannot pass.";
+
+var contactHTML = "For any queries, please fill the form below and click on <b>Send.</b><br /><br />\
+              <label for='to'>To : </label>\
+              <label for='temail'>mohdejazsiddiqui@gmail.com</label><br />\
+              <label for='from'>From : </label>\
+              <input type='text' name='from'><br />\
+              <label for='query'>Query : </label><br />\
+              <textarea cols='50' rows='5' placeholder='Enter your query...''></textarea><br />\
+              <button onclick='#'>Send</button>\
+            </div>\
+        </div>\
+      </div>";
+
+var dict = {
+	"hom" : [homeHTML, "Home"],
+	"adm" : [admissionsHTML, "Admissions"],
+	"syl" : [syllabusHTML, "Syllabus"],
+	"gal" : [galleryHTML, "Gallery"],
+	"hwt" : [hwtHTML, "How We Teach"],
+	"sbl" : [sblHTML, "School Blog"],
+	"cnt" : [contactHTML, "Contact Us"]
+};
+
+function home(id1, id2, ntxt) {
+		var cont1 = document.getElementById(id1);
+		var cont2 = document.getElementById(id2);
+		cont1.innerHTML = dict[ntxt][1];
+		cont2.innerHTML = dict[ntxt][0];
+}
